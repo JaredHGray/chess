5d9edfc4137c82e4c1c4466a0f2e5612f6b56741
JaredHGray <94664698+JaredHGray@users.noreply.github.com>
2024-02-13 11:50:45 -0700

    began implementing registration endpoint

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -2,6 +2,8 @@ package server;
 
 import spark.*;
 import java.nio.file.Paths;
+import com.google.gson.Gson;
+import java.util.*;
 
 public class Server {
 
@@ -19,7 +21,28 @@ public class Server {
     }
 
     private static void createRoutes(){
+        Spark.post("/user", (req, res) -> registerUser(req, res));
 
+    }
+
+    private static String registerUser(Request req, Response res){
+        //get data from the response body
+        String username = req.queryParams("username");
+        String password = req.queryParams("password");
+        String email = req.queryParams("email");
+
+        //check if the user already exists
+
+        //check if valid request
+
+        //create the new user
+
+        //register a new auth token with user
+
+        //return success request
+        res.status(200);
+        res.type("\"application/json\"");
+        return "{\"username\":\"" + username + "\", \"authToken\":\"" + authToken + "\"}";
     }
 
     public void stop() {
