e18583c4cdc9bae0c0592f3ccb7cd18f4e6b73a0
JaredHGray <94664698+JaredHGray@users.noreply.github.com>
2024-02-13 12:25:38 -0700

    created outline of registerUser function

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -20,29 +20,40 @@ public class Server {
         return Spark.port();
     }
 
-    private static void createRoutes(){
+    private void createRoutes(){
         Spark.post("/user", (req, res) -> registerUser(req, res));
 
     }
 
-    private static String registerUser(Request req, Response res){
+    private String registerUser(Request req, Response res){
         //get data from the response body
         String username = req.queryParams("username");
         String password = req.queryParams("password");
         String email = req.queryParams("email");
 
         //check if the user already exists
-
+        if(/**getUser function*/){
+            res.status(403);
+            return "{\"message\": \"Error: already taken\"}";
+        }
         //check if valid request
-
+        if (username == null || username.isEmpty() || password == null || password.isEmpty() || email == null || email.isEmpty()) {
+            res.status(400);
+            return "{\"message\": \"Error: bad request\"}";
+        }
         //create the new user
-
+        /**createUser function*/
         //register a new auth token with user
-
+        String authToken = generateToken();
+        /**createAuth function*/
         //return success request
         res.status(200);
         res.type("\"application/json\"");
         return "{\"username\":\"" + username + "\", \"authToken\":\"" + authToken + "\"}";
+    }
+
+    private String generateToken(){
+        return UUID.randomUUID().toString();
     }
 
     public void stop() {
