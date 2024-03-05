47d7225fddc295ad810c0107f470f1aa57dfe836
JaredHGray <94664698+JaredHGray@users.noreply.github.com>
2024-03-04 20:33:51 -0700

    completed UserDAOTests

--- server/src/test/java/dataAccessTests/UserDAOTests.java
+++ server/src/test/java/dataAccessTests/UserDAOTests.java
@@ -44,41 +44,63 @@ public class UserDAOTests {
         var newUserAgain = userDAO.addUser(newUser);
         Assertions.assertFalse(newUserAgain, "User should not exist in the database");
     }
+    @Test
+    @Order(3)
+    @DisplayName("Find User")
+    public void findUser() throws TestException, DataAccessException {
+        var newUser = new UserData("newUser", "abc123", "nu@gmail.com");
+        userDAO.addUser(newUser);
+        var findUser = userDAO.getUser(newUser);
+        Assertions.assertNotNull(findUser, "User is not found in the database");
+        Assertions.assertEquals(newUser.username(), findUser.username(), "Wrong username found");
+    }
 
-//    @Order(3)
-//    @DisplayName("Login User")
-//    public void loginUser() throws TestException, DataAccessException {
-//        var newUser = new UserData("newUser", "abc123", "nu@gmail.com");
-//        userService.addUser(newUser);
-//        var findUser = userDAO.getUser(newUser);
-//        Assertions.assertNotNull(findUser);
-//        Assertions.assertEquals(newUser.username(), findUser.username());
-//        var newUserLogin = new UserData("newUser", "abc123", null);
-//        var loginResult = userService.loginUser(newUserLogin);
-//        Assertions.assertEquals(200, loginResult.get("code"));
-//    }
-//
-//    @Test
-//    @Order(4)
-//    @DisplayName("Invalid Login")
-//    public void invalidLogin() throws TestException, DataAccessException {
-//        var newUser = new UserData("newUser", "abc123", "nu@gmail.com");
-//        userService.addUser(newUser);
-//        var findUser = userDAO.getUser(newUser);
-//        Assertions.assertNotNull(findUser);
-//        Assertions.assertEquals(newUser.username(), findUser.username());
-//        var newUserLogin = new UserData("newUser", "123abc", null);
-//        var loginResult = userService.loginUser(newUserLogin);
-//        Assertions.assertEquals(401, loginResult.get("code"));
-//    }
+    @Test
+    @Order(4)
+    @DisplayName("Find Non-Existent User")
+    public void findBadUser() throws TestException, DataAccessException {
+        var newUser = new UserData("newUser", "abc123", "nu@gmail.com");
+        userDAO.addUser(newUser);
+        var findUser = userDAO.getUser(newUser);
+        Assertions.assertNotNull(findUser);
+        var newUserLogin = new UserData("fakeUser", "123abc", "apple@gmail.com");
+        var findResult = userDAO.getUser(newUserLogin);
+        Assertions.assertNull(findResult, "This user should not exist");
+    }
 
-//    @Test
-//    @Order(7)
-//    @DisplayName("Clear Users")
-//    public void clearUsers() throws TestException, DataAccessException {
-//        var newUser = new UserData("newUser", "abc123", "nu@gmail.com");
-//        userService.addUser(newUser);
-//        userService.clearUsers();
-//        Assertions.assertNull(userDAO.getUser(newUser));
-//    }
+    @Test
+    @Order(5)
+    @DisplayName("Login User")
+    public void loginUser() throws TestException, DataAccessException {
+        var newUser = new UserData("newUser", "abc123", "nu@gmail.com");
+        userDAO.addUser(newUser);
+        var findUser = userDAO.verifyUser(newUser);
+        Assertions.assertNotNull(findUser, "User is not found in the database");
+        Assertions.assertEquals(newUser.username(), findUser.username(), "Wrong username found");
+        Assertions.assertEquals(newUser.password(), findUser.password(), "Passwords do not match");
+    }
+
+    @Test
+    @Order(6)
+    @DisplayName("Invalid Login")
+    public void invalidLogin() throws TestException, DataAccessException {
+        var newUser = new UserData("newUser", "abc123", "nu@gmail.com");
+        userDAO.addUser(newUser);
+        var findUser = userDAO.verifyUser(newUser);
+        Assertions.assertNotNull(findUser, "User is not found in the database");
+
+        var newUserLogin = new UserData("newUser", "123abc", "apple@gmail.com");
+        var findResult = userDAO.verifyUser(newUserLogin);
+        Assertions.assertNull(findResult, "Passwords do not match");
+    }
+
+    @Test
+    @Order(7)
+    @DisplayName("Clear Users")
+    public void clearUser() throws TestException, DataAccessException {
+        var newUser = new UserData("newUser", "abc123", "nu@gmail.com");
+        userDAO.addUser(newUser);
+        userDAO.clearUsers();
+        Assertions.assertNull(userDAO.getUser(newUser));
+    }
 }
