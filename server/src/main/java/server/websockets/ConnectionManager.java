9e8484b8a950d9ef2e05b5228925689f71fac9c3
JaredHGray <94664698+JaredHGray@users.noreply.github.com>
2024-04-02 10:33:12 -0600

    created started classes for websockets

---
+++ server/src/main/java/server/websockets/ConnectionManager.java
@@ -0,0 +1,12 @@
+package server.websockets;
+import org.eclipse.jetty.websocket.api.Session;
+import webSocketMessages.serverMessages.ServerMessage;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class ConnectionManager {
+    public final ConcurrentHashMap<String, Connection> connections = new ConcurrentHashMap<>();
+
+}
